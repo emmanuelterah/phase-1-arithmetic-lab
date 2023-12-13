b6240d0b01957896121072a13256379b342104b0
emmanuelterah <emmanuelterah98@gmail.com>
2023-12-13 10:24:00 +0300

    Completed the assignment

--- index.js
+++ index.js
@@ -1,2 +1,25 @@
 
 // Write your code here
+let num1 = 2
+let num2 = 31
+
+let multiply = num1 * num2 ;
+console.log(multiply);
+
+
+////RANDOM INTEGER
+let random = 0;
+random ++;
+
+/////MOD
+
+let num3 = 19
+let num4 = 5
+
+let mod = num3 % num4;
+console.log(mod);
+
+/////MAX
+let numbers = [8, 10, 14, 20, 12]; 
+let max = Math.max(...numbers);
+console.log(max);
